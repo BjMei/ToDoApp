1564295830e41cded5526388dcdc11d85f911265
bjoern.meinhardt <bjoern.meinhardt@koerber-supplychain.com>
2025-11-06 16:47:17 +0100

    ToDo hinzugefügt

--- src/main/java/com/mb/todoapp/main/Main.java
+++ src/main/java/com/mb/todoapp/main/Main.java
@@ -5,6 +5,12 @@ import com.mb.todoapp.task.ToDoGUI;
 
 import javax.swing.*;
 
+// ToDo: Listen speichern in einer Datei (CSV)
+// ToDo: Mehrere Listen erstellen Können (Liste 1, Liste 2)
+// ToDo: Zusätzlich eine Prio angeben zum Text (1-5)
+// ToDo: Filterung der Prio (Absteigend)
+// ToDo: Prio farblich hinterlegen (rot, gelb, grün...)
+
 /**
  * Einstiegspunkt der ToDo-Anwendung.
  * <p>
